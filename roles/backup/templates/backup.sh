d6587e94128162dd44128f7dc4e387bda69fcc6d
Martin Goellnitz <martin@goellnitz.de>
2020-01-24 12:50:05 +0100

    In Trouble with Storage Space use slow Compression in the Fly

--- roles/backup/templates/backup.sh
+++ roles/backup/templates/backup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Copyright 2019 Martin Goellnitz.
+# Copyright 2019-2020 Martin Goellnitz.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -19,7 +19,7 @@ SERVER=$1
 HOST=$(grep store.url /opt/coremedia/${SERVER}*-server/*-server.properties|cut -d ' ' -f 3 |sed -e 's/jdbc:mysql:..\(.*\):.*$/\1/g')
 PWD=$(grep store.pass /opt/coremedia/${SERVER}*-server/*-server.properties|grep ssword|cut -d ' ' -f 3-10)
 ROLE=$(grep store.url /opt/coremedia/${SERVER}*-server/*-server.properties|cut -d ' ' -f 3-10|cut -d '/' -f 4)
-mysqldump -p$PWD $ROLE -u $ROLE -h $HOST --set-gtid-purged=OFF > /var/coremedia/backup/$ROLE.sql
 rm -f /var/coremedia/backup/$ROLE.sql.xz
-cat /var/coremedia/backup/$ROLE.sql | parallel --block 256m --pipe --recend '' -k xz -9 > /var/coremedia/backup/$ROLE.sql.xz
+mysqldump -p$PWD $ROLE -u $ROLE -h $HOST --set-gtid-purged=OFF | \
+  xz -cz9 > /var/coremedia/backup/$ROLE.sql.xz
 chmod 644 /var/coremedia/backup/$ROLE.sql.xz
